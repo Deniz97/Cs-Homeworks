e8ad2c1057d647b41dce869d15fdf659ef932284
Deniz97 <d7deniz@windowslive.com>
2017-01-05 01:21:36 +0200

    fdsa

--- Ceng435-Networking/HW2/client.py
+++ Ceng435-Networking/HW2/client.py
@@ -1,9 +1,12 @@
 from netprotocol import *
-from netp_thread import *
+from client_thread import *
 import socket
 import os
 import threading
 import Queue
+import time
+
+time_stamp = time.time()
 
 if not os.path.isfile("./input.txt"):
 	print "Could not locate the file, exiting."
@@ -13,25 +16,27 @@ try :
 except Exception:
 	print "Could not open file input.txt"
 
-def compDestIp(ip):
+def compDestIp(ip,numberOfInterfaces):
 	retval=ip
-	retval=ip[:6]+str(int(ip[6])+3) +ip[7:]
+	retval=ip[:6]+str(int(ip[6])+numberOfInterfaces) +ip[7:]
 	return retval
 
 # Create new threads
 interface_ip_array = findInterfaces()
+interface_ip_array.sort()
+numberOfInterfaces = len(interface_ip_array)
 print interface_ip_array
 thread_array = []
 que_array = []
 i=0
-while i<len(interface_ip_array) :
-	que_array.append( Queue.Queue(6000/len(interface_ip_array))  )
-	my_thread = MainSender("Threadd-"+str(i),interface_ip_array[i] , compDestIp(interface_ip_array[i]),3000+i,que_array[i]  )
+while i<numberOfInterfaces :
+	que_array.append( Queue.Queue(6000/numberOfInterfaces)  )
+	my_thread = MainSender("Threadd-"+str(i),interface_ip_array[i] , compDestIp(interface_ip_array[i],numberOfInterfaces),3000+i,que_array[i]  )
 	my_thread.start()
 	thread_array.append(my_thread)
 	i=i+1
 j=0
-print "ABOUT TO READ THE FILE"
+
 to_push=input_file.read(900)
 while  to_push!="" and j<i:
 	que_array[j].put(to_push)
@@ -41,8 +46,12 @@ while  to_push!="" and j<i:
 		j=0
 		while(j<i):
 			que_array[j].put("break")
+			j+=1
 		break
 	
 
 for t in thread_array:
 	t.join()
+input_file.close()
+
+print time_stamp
